f1c920305fd2ab95e2c342123c55c2fffd1cba8e
David Bauer <mail@david-bauer.net>
2023-05-07 20:10:21 +0200

    wda: aggregation

--- commands-wda.h
+++ commands-wda.h
@@ -21,10 +21,16 @@
 
 #define __uqmi_wda_commands \
 	__uqmi_command(wda_set_data_format, wda-set-data-format, required, QMI_SERVICE_WDA), \
+	__uqmi_command(wda_data_aggregation_protocol, aggregation-protocol, required, CMD_TYPE_OPTION), \
+	__uqmi_command(wda_downlink_data_aggregation_max_datagrams, dl-datagram-max-count, required, CMD_TYPE_OPTION), \
+	__uqmi_command(wda_downlink_data_aggregation_max_size, dl-datagram-max-size, required, CMD_TYPE_OPTION), \
 	__uqmi_command(wda_get_data_format, wda-get-data-format, no, QMI_SERVICE_WDA)
 
 
 #define wda_helptext \
 		"  --wda-set-data-format <type>:     Set data format (type: 802.3|raw-ip)\n" \
+		"    --aggregation-protocol <proto>: Set aggregation protocol (proto: tlp|qc-cm|mbim|rndis|qmap)\n" \
+		"    --dl-datagram-max-count <size>: Set downlink aggregation max datagrams (number)\n" \
+		"    --dl-datagram-max-size <size>:  Set downlink aggregation max datagram size (number)\n" \
 		"  --wda-get-data-format:            Get data format\n" \
 
